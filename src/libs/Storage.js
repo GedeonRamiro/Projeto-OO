cc3d7658fb07dc27f77dc6426a9ea9884824f2bc
Gedeon Ramiro <gedeon_ramiro@hotmail.com>
2022-08-06 09:42:27 -0300

    feat: create function save

--- src/libs/Storage.js
+++ src/libs/Storage.js
@@ -1,5 +1,34 @@
-class Storage {
-  async save() {}
+export class Storage {
+  #table;
+
+  constructor(table) {
+    this.#table = table;
+  }
+
+  get table() {
+    return this.#table;
+  }
+
+  set table(table) {
+    return (this.#table = table);
+  }
+
+  async save(data) {
+    const id = this.#generateId();
+    const key = `${this.#table}-${id}`;
+
+    const newData = {
+      id,
+      ...data,
+    };
+
+    localStorage.setItem(key, JSON.stringify(newData));
+  }
+
+  #generateId() {
+    const gen = () => Math.floor(Date.now() * Math.random()).toString(36);
+    return `${gen()}-${gen()}-${gen()}`;
+  }
 
   async findOne(id) {}
 
