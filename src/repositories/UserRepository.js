6eb4c48144298712d239e566e36dc0138aa4531c
Gedeon Ramiro <gedeon_ramiro@hotmail.com>
2022-08-09 10:39:58 -0300

    feat: create funciton save | getById | update

--- src/repositories/UserRepository.js
+++ src/repositories/UserRepository.js
@@ -6,8 +6,23 @@ export class UserRepository {
     this.#LStorage = new Storage("user");
   }
 
-  async save(data) {
-    const user = await this.#LStorage.save(data);
+  async save(user) {
+    const newUser = await this.#LStorage.save(user.toObject());
+    return new User(newUser);
+  }
+
+  async getById(id) {
+    const user = await this.#LStorage.findOne(id);
+    if (!user) throw new Error(`Usuário ${id} não encontrado!`);
     return new User(user);
+  }
+
+  async getAll() {
+    const users = await this.#LStorage.findAll();
+    return users.map((user) => new User(user));
+  }
+
+  async update(user) {
+    return this.#LStorage.findOneAndUpdate(user.id, user.toObject());
   }
 }
