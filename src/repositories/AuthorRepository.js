948d3549a46862f951ed86062b30cceada3d3c0b
Gedeon Ramiro <gedeon_ramiro@hotmail.com>
2022-08-10 10:08:27 -0300

    refactor: extends class Repository

--- src/repositories/AuthorRepository.js
+++ src/repositories/AuthorRepository.js
@@ -1,36 +1,24 @@
+import { Repository } from "./Repository.js";
 import { Author } from "../entities/Author.js";
 
-export class AuthorRepository {
-  #LStorage;
+export class AuthorRepository extends Repository {
   constructor(Storage) {
-    this.#LStorage = new Storage("author");
+    super(Storage);
+    this.setStorageTable("user");
   }
 
   async save(author) {
-    const newAuthor = await this.#LStorage.save(author.toObject());
+    const newAuthor = await super.save(author);
     return new Author(newAuthor);
   }
 
   async getById(id) {
-    const author = await this.#LStorage.findOne(id);
-    if (!author) throw new Error(`Autor ${id} não encontrado!`);
+    const author = await super.getById(id);
     return new Author(author);
   }
 
   async getAll() {
-    const authors = await this.#LStorage.findAll();
+    const authors = await super.getAll();
     return authors.map((author) => new Author(author));
-  }
-
-  async update(author) {
-    return this.#LStorage.findOneAndUpdate(author.id, author.toObject());
-  }
-
-  async remove(id) {
-    return this.#LStorage.remove(id);
-  }
-
-  async removeAll() {
-    return this.#LStorage.removeAll();
   }
 }
